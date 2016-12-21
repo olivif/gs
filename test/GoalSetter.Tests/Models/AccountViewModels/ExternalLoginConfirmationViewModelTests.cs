b576831525243f94c6195cb4bc38f382858c2059
Olivia <olivia_git@outlook.com>
2016-12-21 17:23:06 +0200

    Adding model tests.

---
+++ test/GoalSetter.Tests/Models/AccountViewModels/ExternalLoginConfirmationViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿namespace GoalSetter.Models.AccountViewModels
+{
+    using Xunit;
+
+    public class ExternalLoginConfirmationViewModelTests
+    {
+        [Fact]
+        public void ConstructorDefault()
+        {
+            // Act
+            new ExternalLoginConfirmationViewModel();
+        }
+
+        [Fact]
+        public void ConstructorParams()
+        {
+            // Act
+            var email = "email";
+            var model = new ExternalLoginConfirmationViewModel()
+            {
+                Email = email
+            };
+        }
+
+        [Fact]
+        public void Email()
+        {
+            // Arrange
+            var email = "email";
+            var model = new ExternalLoginConfirmationViewModel()
+            {
+                Email = email
+            };
+
+            // Act
+            var foundEmail = model.Email;
+
+            // Assert
+            Assert.Equal(email, foundEmail);
+        }
+    }
+}
