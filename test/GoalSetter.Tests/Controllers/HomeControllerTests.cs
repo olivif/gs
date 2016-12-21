8771bc16f4e84efa6cac3485b09cfd3b6377ce13
Olivia <olivia_git@outlook.com>
2016-12-21 16:55:47 +0200

    Fixing unit test project reference issues.

---
+++ test/GoalSetter.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace GoalSetter
+{
+    using Controllers;
+    using Xunit;
+
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index() 
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            controller.Index();
+        }
+    }
+}
