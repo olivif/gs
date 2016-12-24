58613313b0e07dee070a294f2f479de4a3a0e6cc
Olivia <olivia_git@outlook.com>
2016-12-24 13:17:57 +0200

    Adding goals db context and tests.

---
+++ src/GoalSetter/Data/GoalsDbContext.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="GoalsDbContext.cs" company="olivif">
+// Copyright (c) olivif 2016
+// </copyright>
+
+namespace GoalSetter.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models.Goals;
+
+    /// <summary>
+    /// Database context for goals data
+    /// </summary>
+    public class GoalsDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GoalsDbContext"/> class.
+        /// </summary>
+        /// <param name="options">Db context options</param>
+        public GoalsDbContext(DbContextOptions<GoalsDbContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the goals
+        /// </summary>
+        public DbSet<GoalViewModel> Goals { get; set; }
+    }
+}
---
+++ test/GoalSetter.Tests/Data/GoalsDbContextTests.cs
@@ -0,0 +1,37 @@
+﻿namespace GoalSetter.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models.Goals;
+    using Xunit;
+
+    public class GoalsDbContextTests
+    {
+        private readonly GoalsDbContext dbContext;
+
+        public GoalsDbContextTests()
+        {
+            var options = new DbContextOptions<GoalsDbContext>();
+            this.dbContext = new GoalsDbContext(options);
+        }
+
+        [Fact]
+        public void Constructor()
+        {
+            // Arrange
+            var options = new DbContextOptions<GoalsDbContext>();
+
+            // Act
+            var dbContext = new GoalsDbContext(options);
+        }
+
+        [Fact]
+        public void HasGoals()
+        {
+            // Act
+            var goalsDbSet = this.dbContext.Goals;
+
+            // Assert
+            Assert.True(goalsDbSet is DbSet<GoalViewModel>);
+        }
+    }
+}
