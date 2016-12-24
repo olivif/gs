23234b96ce73196c86a0e1a5df686b3d66dbc4de
Olivia <olivia_git@outlook.com>
2016-12-24 11:49:25 +0200

    Adding test goal data model.

---
+++ src/GoalSetter/Models/Goals/Goal.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Goal.cs" company="olivif">
+// Copyright (c) olivif 2016
+// </copyright>
+
+namespace GoalSetter.Models.Goals
+{
+    /// <summary>
+    /// Goal data model
+    /// </summary>
+    public class Goal
+    {
+        /// <summary>
+        /// Gets or sets the user id 
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the goal data
+        /// </summary>
+        public string Data { get; set; }
+    }
+}
